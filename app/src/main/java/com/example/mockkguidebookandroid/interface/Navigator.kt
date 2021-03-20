1019a322884cb6480123f443d1cb353300c27f00
Cthoughtz <cthoughtz@gmail.com>
2021-03-20 01:18:33 -0500

    Example stubbing functions

---
+++ app/src/main/java/com/example/mockkguidebookandroid/interface/Navigator.kt
@@ -0,0 +1,8 @@
+package com.example.mockkguidebookandroid.`interface`
+
+interface Navigator {
+
+    val currentLocation:String
+
+    fun navigateTo(newLocation:String)
+}
---
+++ app/src/test/java/com/example/interface/NavigatorTest.kt
@@ -0,0 +1,25 @@
+package com.example.`interface`
+
+import com.example.mockkguidebookandroid.`interface`.Navigator
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.Test
+
+class NavigatorTest {
+
+    val navigator = mockk<Navigator>()
+
+    @Test
+    fun exampleTest(){
+        //Stubbing allows you to setup canned answers when functions are called
+        every { navigator.currentLocation } returns "Home"  // <--- Stub
+        every { navigator.navigateTo("Part")} throws  // <-- Stub
+                IllegalAccessException("Can't reach the park")
+
+        //prints "Home"
+        println(navigator.currentLocation)
+
+        // throws an IllegalStateException
+        navigator.navigateTo("Park")
+    }
+}
